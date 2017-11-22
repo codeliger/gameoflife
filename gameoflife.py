b3460c2fbca7b8d1d88b02b082f08d832fa3b7dd
Ben Tiessen <bentiessen@gmail.com>
2017-11-21 22:21:25 -0600

    Before recursive attempt

--- gameoflife.py
+++ gameoflife.py
@@ -5,23 +5,17 @@ class Universe:
         self.neighbours = [(x,y) for x in range(-1,2) for y in range(-1,2) if not (x == 0 and y == 0)]
 
     def __iter__(self):
-        self.index.__iter__()
-
-    def get_cell_at(self, position:tuple):
-        return self.index[position]
+        return self.index.__iter__()
 
     def has_cell_at(self,position:tuple):
         return position in self.index
 
-    def revive_cell_at(self,position:tuple):
-        if position in self.index:
-            self.index[position] = True
+    def get_cell_at(self, position:tuple):
+        value = self.index[position]
+        if value is None:
+            raise Exception("Could not revive a cell at {0}".format(value))
         else:
-            raise Exception("Could not revive a cell at {0} because it doesn't exist.".format(position))
-
-    def kill_cell_at(self,position:tuple):
-        if position in self.index:
-            self.index[position] = False
+            return value
 
     def create_cell_at(self,position:tuple):
         if self.has_cell_at(position):
@@ -29,26 +23,72 @@ class Universe:
         self.index[position] = True
 
     def destroy_cell_at(self,position:tuple):
-        if position in self.index:
+        if self.has_cell_at(position):
             self.index.pop(position)
         else:
             raise Exception("Could not destroy a cell at {0} because it doesn't exist.".format(position))
 
-    def get_cell_neighbours(self,position:tuple)->list:
-        return [tuple([position[0]+neighbour[0],position[1]+neighbour[1]]) for neighbour in self.neighbours]
+    def revive_cell_at(self,position:tuple):
+        if self.has_cell_at(position):
+            self.index[position] = True
+        else:
+            raise Exception("Could not revive a cell at {0} because it doesn't exist.".format(position))
 
-    def get_cell_neighbour_states(self,cell_neighbours:list)->list:
-        return [self.return_cell_at(position) for position in cell_neighbours]
+    def kill_cell_at(self,position:tuple):
+        if self.has_cell_at(position):
+            self.index[position] = False
+        else:
+            raise Exception("Could not kill cell at {0} because it doesn't exist.".format(position))
 
-    def count_alive_cell_neighbours(self,cell_neighbour_states:list)->int:
-        return cell_neighbour_states.count(True)
+
+
 
 
 if __name__ == '__main__':
-    u = Universe()
-    u.create_cell_at((0,1))
-    u.create_cell_at((0,2))
-    u.create_cell_at((0,3))
+    universe = Universe()
+    universe_b = Universe()
+    universe.create_cell_at((0,1))
+    universe.create_cell_at((0,2))
+    universe.create_cell_at((0,3))
+
+    def getCellState(location:tuple):
+        neighbour_count = 0
+        dead_cells_to_create = list()
+        for neighbour in universe.neighbours:
+            neighbour = tuple([cell[0] + neighbour[0], cell[1] + neighbour[1]])
+            if universe.has_cell_at(neighbour):
+                neighbour_state = universe.get_cell_at(neighbour)
+                if neighbour_state == True:
+                    neighbour_count+=1
+            else:
+                dead_cells_to_create.append(neighbour)
+        cell_state = neighbour_count > 1 and neighbour_count < 4
+        if cell_state:
+            return map(getCellState,set(dead_cells_to_create))
+        return list(cell)
+
+
+    #evaluate dead cells
+    for cell in universe:
+        if universe.index[cell] == False:
+            neighbour_count = 0
+            for neighbour in universe.neighbours:
+                neighbour = tuple([cell[0] + neighbour[0], cell[1] + neighbour[1]])
+                if universe.has_cell_at(neighbour):
+                    neighbour_state = universe.get_cell_at(neighbour)
+                    if neighbour_state == True:
+                        neighbour_count+=1
+                else:
+                    universe.create_cell_at(neighbour)
+                    universe.kill_cell_at(neighbour)
+            cell_state = neighbour_count > 1 and neighbour_count < 4
+            if cell_state:
+                universe_b.create_cell_at(cell)
+
+
+
+
+
 
 
 
