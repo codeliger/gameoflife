d3724685d65aad6362c3b25e238b6ba99312dd06
Ben Tiessen <bentiessen@gmail.com>
2017-12-18 10:03:05 -0600

    added comments

--- challenege.py
+++ challenege.py
@@ -2,18 +2,20 @@ from math import ceil,floor
 import operator,datetime
 
 def percentile(p,n):
-  return p / 100 * (n+1)
+  return (p / 100) * (n+1)
 
+# 1 = 1, 1.2 = 1, 1.25 = 1.5, 1.7 = 1.5, 1.75 = 2
 def half_round(n):
     fraction = n % 1
-    if fraction > 0.5:
+    if fraction == 0:
+      rounded = n
+    elif fraction >= 0.75:
       rounded = ceil(n)
-    elif fraction < 0.5:
+    elif fraction < 0.25:
       rounded = floor(n)
     else:
-      rounded = n
-
-
+      rounded = (n - n % 1) + .5
+    return rounded
 
 counters = dict()
 
@@ -25,11 +27,11 @@ terminate = False
 
 last_ts = 0
 minute_ts = 0
+count = 0
 
 while terminate == False:
 
     raw_ts = input('Timestamp:')
-    
     if raw_ts == '':
       terminate = True
       continue
@@ -65,18 +67,28 @@ while terminate == False:
     rpt_index = (rounded_rpt * 10) / 5
 
     counters[rpt_index] += 1
+    count += 1
 
 # terminated
 
-#calculate percentile
-total_requests = sum(counters)
-sorted_counters = sorted(counters.items(), key=operator.itemgetter(1))
-print(counters)
-percentile_index = half_round(percentile(90,total_requests))
-
 iso8601 = datetime.datetime.fromtimestamp(minute_ts).isoformat()
-print(iso8601,counters[percentile_index])
 
+# create a list of only the values greater than 0 (possibly more effecient, maybe not)
+active_counters = [(x,counters[x]) for x in counters if counters[x] > 0 ]
+active_counters.sort(key=lambda tup: tup[1])
+
+# count total requests
+percentile_index = half_round(percentile(90,count))
+
+# use the natural logic of the ordered list to find which counter bracket the percentile falls into
+bracket = 0
+for counter in active_counters:
+  bracket += counter[1]
+  if bracket >= percentile_index:
+    print(iso8601,(counter[0]*5)/10)
+    exit()
+    
+    
 
 
 
