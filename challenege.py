fe52ae93c8a62c584d5d753679e5d3dec23fb894
Ben Tiessen <bentiessen@gmail.com>
2017-12-20 14:26:09 -0600

    Solution is working

--- challenege.py
+++ challenege.py
@@ -1,10 +1,11 @@
-from math import ceil,floor
-import operator,datetime
+import datetime
+from math import ceil, floor
 
-def percentile(p,n):
-  return (p / 100) * (n+1)
 
-# 1 = 1, 1.2 = 1, 1.25 = 1.5, 1.7 = 1.5, 1.75 = 2
+def percentile(percentile,number_of_elements):
+  return (percentile / 100) * number_of_elements
+
+'''Rounds all numbers to whole, whole or whole and a half numbers'''
 def half_round(n):
     fraction = n % 1
     if fraction == 0:
@@ -17,96 +18,79 @@ def half_round(n):
       rounded = (n - n % 1) + .5
     return rounded
 
-counters = dict()
+def percentile_of_dict(counters:dict,minute_timestamp):
+  iso8601 = datetime.datetime.fromtimestamp(minute_timestamp).isoformat()
 
-# 300 * 5 = 1500 meaning each 5 represents .5 miliseconds
-for i in range(1,301,1):
-  counters[i] = 0
+  # create a list of only the values greater than 0 (possibly more effecient, maybe not)
+  active_counters = [(x,counters[x]) for x in counters if counters[x] > 0 ]
+  active_counters.sort(key=lambda tup: tup[1])
 
-last_ts = 0
-minute_ts = 0
-count = 0
-
-while True:
-
-    raw_input = input("ts rpt:")
-
-    if raw_input == '':
+  # count total requestimestamp
+  percentile_index = percentile(90,count)
+  fraction = percentile_index % 1
+  bracket = 0
+  for counter in active_counters:
+    bracket += counter[1]
+    if bracket >= percentile_index:
+      print(iso8601+'Z',(counter[0] * 5) / 10)
       break
 
-    split_input = raw_input.split(' ')
+counters = dict()
+# 300 * 5 = 1500 meaning each 5 representimestamp .5 miliseconds
+for i in range(0,301,1):
+  counters[i] = 0
 
-    raw_ts = split_input[0] #timestamp
-    raw_rpt = split_input[0] #request processing time
-    
-    ts = float(raw_ts)
-    rpt = float(raw_rpt)
+previous_timestamp = None #the previous timestamp stored, to compare difference in seconds
+minute_timestamp = None # the first timestamp on the minute for every group of resultimestamp
+count = 0 # how many requestimestamp in the log file total
 
-    if rpt == 0 or rpt > 150:
-      continue
+input_found = True
 
-    # if its the first request 
-    if last_ts == 0:
-      # if its not the first request of the timestamps minute keep skipping
-      if ts % 60 != 0:
+while input_found:
+  
+    try:
+        raw = input()
+    except EOFError:
+        input_found = False
+    if raw == '':
+        input_found = False
+
+    if input_found:
+      split_request = raw.split(' ')
+      raw_timestamp = split_request[0] #timestamp
+      raw_request_processing_time = split_request[1] #request processing time
+      timestamp = float(raw_timestamp)
+      request_processing_time = float(raw_request_processing_time)
+      if request_processing_time < 0 or request_processing_time > 150:
         continue
-      minute_ts = ts
-      last_ts = ts
-
-    # if request doesnt belong to minute discard it
-    if ts - last_ts > 60:
-        continue
-
-    rounded_rpt = half_round(rpt)
-
-    # calculate the index between 1 and 300
-    rpt_index = (rounded_rpt * 10) / 5
-
-    counters[rpt_index] += 1
-    count += 1
-
-# terminated
-
-if count == 0:
-  print('Exiting')
-  exit()
-
-try:
-  iso8601 = datetime.datetime.fromtimestamp(minute_ts).isoformat()
-except Exception as e:
-  print('minute_ts:',minute_ts,e)
-
-
-# create a list of only the values greater than 0 (possibly more effecient, maybe not)
-active_counters = [(x,counters[x]) for x in counters if counters[x] > 0 ]
-active_counters.sort(key=lambda tup: tup[1])
-
-# count total requests
-percentile_index = half_round(percentile(90,count))
-
-# use the natural logic of the ordered list to find which counter bracket the percentile falls into
-bracket = 0
-for counter in active_counters:
-  bracket += counter[1]
-  if bracket >= percentile_index:
-    print(iso8601,(counter[0] * 5) / 10)
-    exit()
-    
-    
-
-
-
-    
-
-
-      
-
-    
-
-
-
-
-
-
-    
-
+      if minute_timestamp is None: # no minute found yet
+        if timestamp % 60 == 0: # beginning of minute found
+          print("setting minute timestamp for first time",timestamp)
+          minute_timestamp = timestamp
+      else:
+        # minute found and previously set
+        if minute_timestamp != timestamp and timestamp % 60 == 0:
+          print('starting new minute',timestamp)
+          percentile_of_dict(counters,minute_timestamp)
+          minute_timestamp = timestamp
+          #reset counters for next minute
+          for c in counters:
+            counters[c] = 0
+          count = 0
+      #first timestamp inputted
+      if previous_timestamp is None:
+        previous_timestamp = timestamp
+      else:
+        #discard requestimestamp not related to the minute being evaluated
+        if timestamp - previous_timestamp > 60:
+          continue 
+      rounded_request_processing_time = half_round(request_processing_time)
+      # calculate the index between 1 and 300
+      request_processing_time_index = (rounded_request_processing_time * 10) / 5
+      counters[request_processing_time_index] += 1
+      count += 1
+      previous_timestamp = timestamp
+    elif count > 0:
+      percentile_of_dict(counters,minute_timestamp)
+      exit()
+          
