86a457bc20c965372c5a73a19619a80acb663aec
Ben Tiessen <bentiessen@gmail.com>
2017-12-21 19:36:19 -0600

    working, before trying a more effecient approach

--- challenge.py
+++ challenge.py
@@ -1,104 +1,142 @@
 import datetime
 from math import ceil, floor
 
+'''convert unix timestamp to iso time'''
+def ts_to_iso(ts):
+    return datetime.datetime.fromtimestamp(ts).isoformat()
 
+
+''' calulates a percentile according to wikipedia'''
 def percentile(percentile, number_of_elements):
-  return (percentile / 100) * number_of_elements
+    return (percentile / 100) * number_of_elements
 
-'''Rounds all numbers to whole, whole or whole and a half numbers'''`
+'''Rounds everything to a whole number or a whole number plus .5'''
 def half_round(n):
     fraction = n % 1
     if fraction == 0:
-      rounded = n
+        rounded = n
     elif fraction >= 0.75:
-      rounded = ceil(n)
+        rounded = ceil(n)
     elif fraction < 0.25:
-      rounded = floor(n)
+        rounded = floor(n)
     else:
-      rounded = (n - n % 1) + .5
+        rounded = (n - n % 1) + .5
+    # print('Rounding',n,rounded)
     return rounded
 
-def percentile_of_dict(counters:dict, minute_timestamp):
+''' applies a percentile to a dictionary of values
+ and iterates until the percentiles bracket is found '''
+def percentile_of_dict(counters: dict, count: int):
+    active_counters = [(x, counters[x]) for x in counters if counters[x] > 0]
+    tmp_count = 0
+    for ind,counter in enumerate(active_counters):
+        tmp_count ++ counter[1]
+        # print(counter[0],'has',tmp_count)
+    percentile_index = percentile(90, count)
+    # print('calculated percentile:',percentile_index)
+    bracket = 0
+    # aggregate each amount of requests per response time until the number is
+    # in the percentile calculated on the total amount of requests
+    for idx,counter in enumerate(active_counters):
+        bracket += counter[1]
+        # print(idx,'index',counter[0],'is for',(counter[0]*5)/10,'searching with',percentile_index,'in',bracket)
+        if bracket >= percentile_index:
+            # print('found bracket',bracket,percentile_index)
+            return (counter[0] * 5) / 10
+    raise ValueError('The percentile category could not be found in the dictionary.')
 
-  iso8601 = datetime.datetime.fromtimestamp(minute_timestamp).isoformat()
-  # create a list of only the values greater than 0 (possibly more effecient, maybe not)
-  active_counters = [(x, counters[x]) for x in counters if counters[x] > 0 ]
-  active_counters.sort(key=lambda tup: tup[1])
-  percentile_index = percentile(90,count)
-  fraction = percentile_index % 1
-  bracket = 0
-  # aggregate each amount of requests per response time until the number is within the percentile calculated on the total amount of requests
-  for counter in active_counters:
-    bracket += counter[1]
-    if bracket >= percentile_index:
-      print(iso8601+'Z',(counter[0] * 5) / 10)
-      break
-
+''' creates a dict with 300 values representing
+ 0 to 150 with .5 increments in between '''
 def initialize_counter():
-  requests_count = dict()
-  # 300 * 5 = 1500 meaning each 5 representimestamp .5 miliseconds
-  for i in range(0,301,1):
-    requests_count[i] = 0
-  return requests_count
+    requests_count = dict()
+    for i in range(0, 301, 1):
+        requests_count[i] = 0
+    return requests_count
+
+''' Converts a line from a log file into a
+timestamp and request processing time'''
+def process_request(raw_request: str)->tuple:
+    split_request = raw_request.split(' ')
+    raw_ts = split_request[0]
+    raw_rpt = split_request[1]
+    return float(raw_ts), float(raw_rpt)
+
+''' Forms a string that contains the minute timestamp
+ and 90th percentile of response processing time '''
+def get_timestamp_percentile(requests_count, count, minute_ts):
+    percentile = percentile_of_dict(requests_count, count)
+    iso = ts_to_iso(minute_ts)
+    return '{0}Z {1}'.format(iso, percentile)
+
+''' Takes lines of user input and processes request processing time'''
+def process_request_log():
+    requests_count = initialize_counter()
+    # the previous ts stored, to compare difference in seconds
+    previous_ts = None
+    # the first ts on the minute for every group of results
+    minute_ts = None
+    # how many requests in the log file total
+    count = 0
+    # Terminates the program when input is not recieved
+    input_found = True
+
+    request_outputs = []
+
+    while input_found:
+        try:
+            raw = input()
+        except EOFError:
+            input_found = False
+
+        if raw == '':
+            input_found = False
+
+        try:
+            ts, rpt = process_request(raw)
+        except:
+            input_found = False
+
+        if input_found:
+            if rpt < 0 or rpt > 150:
+                # print('discarding response time of',rpt)
+                continue
+
+            if minute_ts is None:  # no minute found yet
+                if ts % 60 == 0:  # beginning of minute found
+                    minute_ts = ts
+                    # print('starting first minute')
+            else:
+                # minute found and previously set
+                if minute_ts != ts and ts % 60 == 0:
+                    # print('ending minute')
+                    # calculate percentile and output
+                    request_outputs.append(get_timestamp_percentile(requests_count, count, minute_ts))
+                    minute_ts = ts # set new minute 
+                    # reset counters for next minute
+                    for c in requests_count:
+                        requests_count[c] = 0
+                    count = 0
+            # first ts inputted
+            if previous_ts is None:
+                # print('setting previous_ts')
+                previous_ts = ts
+            elif abs(ts - previous_ts) > 60: # discard requests not related to the minute being evaluated
+                # print('discarding',ts,previous_ts,ts-previous_ts)
+                continue
+            rounded_rpt = half_round(rpt)
+            # calculate the index between 1 and 300
+            rpt_index = (rounded_rpt * 10) / 5
+            # print('incrementing',rounded_rpt,rpt_index)
+            requests_count[rpt_index] += 1
+            count += 1
+            previous_ts = ts
+            # print(count)
+        elif count > 0:
+            # print('no input but count',count)
+            request_outputs.append(get_timestamp_percentile(requests_count, count, minute_ts))
+    return request_outputs
 
 if __name__ == '__main__':
-
-  previous_timestamp = None #the previous timestamp stored, to compare difference in seconds
-  minute_timestamp = None # the first timestamp on the minute for every group of resultimestamp
-  count = 0 # how many requestimestamp in the log file total
-
-  input_found = True # Terminates the program when input is not recieved
-
-  while input_found:
-      try:
-          raw = input()
-      except EOFError:
-          input_found = False
-      if raw == '':
-          input_found = False
-
-      if input_found:
-        split_request = raw.split(' ')
-        raw_timestamp = split_request[0] #timestamp
-        raw_request_processing_time = split_request[1] #request processing time
-        
-        try:
-          timestamp = float(raw_timestamp)
-          request_processing_time = float(raw_request_processing_time)
-        except:
-          continue
-
-        if request_processing_time < 0 or request_processing_time > 150:
-          continue
-
-        if minute_timestamp is None: # no minute found yet
-          if timestamp % 60 == 0: # beginning of minute found
-            print("setting minute timestamp for first time",timestamp)
-            minute_timestamp = timestamp
-        else:
-          # minute found and previously set
-          if minute_timestamp != timestamp and timestamp % 60 == 0:
-            #calculate percentile and output
-            percentile_of_dict(counters,minute_timestamp)
-            minute_timestamp = timestamp
-            #reset counters for next minute
-            for c in counters:
-              counters[c] = 0
-            count = 0
-        #first timestamp inputted
-        if previous_timestamp is None:
-          previous_timestamp = timestamp
-        else:
-          #discard requestimestamp not related to the minute being evaluated
-          if timestamp - previous_timestamp > 60:
-            continue 
-        rounded_request_processing_time = half_round(request_processing_time)
-        # calculate the index between 1 and 300
-        request_processing_time_index = (rounded_request_processing_time * 10) / 5
-        counters[request_processing_time_index] += 1
-        count += 1
-        previous_timestamp = timestamp
-      elif count > 0:
-        percentile_of_dict(counters,minute_timestamp)
-        exit()
-            
+    requests = process_request_log()
+    for request in requests:
+        print(request)
--- challenge_generator.py
+++ challenge_generator.py
@@ -1,38 +1,24 @@
-import random, sys
-import challenge
-tsfile = open('timestamp.txt','w')
+from random import randint,uniform
+
+f = open('timestamp.txt','w')
+
 start_ts = 962668800
+last_ts = 962668800
+minute_count = 1
 
-def output(p,r):
-    return ('{0} {1}\n'.format(r,challenge.half_round(p)))
-
-temp_ts = None
-for x in range (1,11):
-    for i in range(1,11):
-        p = random.uniform(0,200) # extra room for invalid input
-        if i == 10:
-            start_ts += 60 - (start_ts % 60)
-            print(output(p,start_ts))
-            tsfile.write(output(p,start_ts))
-        elif temp_ts is not None:
-            print(output(p,temp_ts))
-            tsfile.write(output(p,temp_ts))
-            temp_ts = None
-        else:
-            print(output(p,start_ts))
-            tsfile.write(output(p,start_ts))
-
-        r = random.randint(1,100)
-        if r <= 40 and start_ts % 60 > 1:
-            # decrement second by 1
-            sub =  random.randint(1,(start_ts % 60)-1)
-            temp_ts = start_ts - sub
-        else:
-            # increment second by 1
-            start_ts += 1
-
-
-        
+while minute_count <= 10:
+    delay = round(uniform(0,155),1)
+    print(start_ts,delay)
+    f.write('{0} {1}\n'.format(start_ts,delay))
+    last_ts = start_ts
+    seconds = start_ts % 60
+    if seconds == 0:
+        minute_count += 1
+        start_ts += randint(1,60)
+    else:
+        remaining = 60 - seconds
+        start_ts += randint(-1 * (seconds-1),remaining)
+    
 
 
         
